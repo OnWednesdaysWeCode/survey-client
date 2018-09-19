21f411a98b59b11e15a9919cd339ceed2b803a16
Jen Fox <jenniferfox4@gmail.com>
2018-09-19 15:40:22 -0400

    Added response.user to store in order to store token and log out

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,40 @@
+const store = require('./../store.js')
+const config = require('./../config.js')
+
+const signUpSuccess = function () {
+}
+
+const signUpFail = function (response) {
+
+}
+
+const signInSuccess = function (response) {
+  store.user = response.user
+}
+
+const signInFail = function () {
+}
+
+const changePasswordSuccess = function () {
+
+}
+
+const changePasswordFail = function () {
+}
+
+const logOutSuccess = function () {
+}
+
+const logOutFail = function () {
+}
+
+module.exports = {
+  signUpSuccess,
+  signInSuccess,
+  changePasswordSuccess,
+  logOutSuccess,
+  signInFail,
+  signUpFail,
+  logOutFail,
+  changePasswordFail
+}
