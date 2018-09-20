ceeec09219f347878de1ab7001998f2560e8771f
Jen Fox <33559116+jenfox4@users.noreply.github.com>
2018-09-20 10:14:43 -0400

    Revert "Colors"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const authEvents = require('./auth/events.js')
-const surveyEvents = require('./survey/events.js')
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+
+// use require without a reference to ensure a file is bundled
+// require('./example')
 
 $(() => {
-  authEvents.addHandlers()
-  surveyEvents.addHandlers()
+  // your JS code goes here
 })
