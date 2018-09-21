b3ad82de88563957e33d92c7a394b69dab3872f2
acalish <acalish@gmail.com>
2018-09-21 16:16:45 -0400

    Edit config
    - edit production link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://serene-garden-25502.herokuapp.com/',
+  production: 'https://serene-garden-25502.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
